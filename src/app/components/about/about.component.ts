56a4bdfae18e691c2650e64f84060dbf7231cafb
Valeria Meza <valeriameza173@gmail.com>
2021-03-29 17:03:53 -0500

    add interface for Poke info and render info in cards.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,18 +1,34 @@
+import { PokemonInfo } from './../../models/pokemon.interface';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-  routeParam: string;
+  private routeId: string;
+  pokemonObservable: Observable<PokemonInfo>;
 
-  constructor(private router: ActivatedRoute) { }
+  constructor(private router: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.routeParam = this.router.snapshot.params.id;
+    this.routeId = this.router.snapshot.params.id;
+    this.initPokemonData();
   }
 
+  // private async initPokemonData(): Promise<void> {
+  //   const data = await this.http
+  //     .get<PokemonInfo>(`https://pokeapi.co/api/v2/pokemon/${this.routeId}`)
+  //     .toPromise();
+  //   console.log('data', data);
+  // }
+
+  private initPokemonData(): void {
+    this.pokemonObservable = this.http
+      .get<PokemonInfo>(`https://pokeapi.co/api/v2/pokemon/${this.routeId}`);
+  }
 }
